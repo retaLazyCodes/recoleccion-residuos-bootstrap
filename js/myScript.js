1229bba81b7b6c8d5dec614351049b854fb5fe16
Brian Retamar <leo1632@hotmail.com>
2021-10-06 19:08:06 -0300

    se agrego la mensajeria

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,27 @@
 
-const btn = document.getElementById('weajajadejalo')
+// Cuando se haga click en el avatar del modal, se abre un file dialog para cambiar el avatar
+const accountModalAvatar = document.querySelector('.accountModalAvatar')
+const changeAvatarBtn = document.querySelector('#changeAvatar')
 
-btn.addEventListener('click', function () {
-    window.alert('notevamos a dar jaja salu2')
+accountModalAvatar.addEventListener('click', function () {
+    changeAvatarBtn.click()
+})
+
+
+
+// Cuando se haga click en el boton 'Editar' del modal, se activan los inputs que estaban desactivados
+const editUserBtn = document.querySelector('#editUserBtn')
+
+// crea un array con todos los elementos que tengan la clase 'accountModalInput'
+const refs = [...document.querySelectorAll('.accountModalInput')];
+
+// cuando se hace click en el boton 'Editar' ...
+editUserBtn.addEventListener('click', function () {
+    // ... Ejecuto esto
+    console.log(refs)
+    refs.forEach(input => {
+        input.removeAttribute('disabled')
+    })
+    accountModalAvatar.scrollIntoView()
+    refs[0].focus()
 })
